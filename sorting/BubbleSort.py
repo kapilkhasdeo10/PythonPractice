c42bf5d33528672eca0384cb50d68c8a6e5c13f8
Garima <garimakhasdeo1610@gmail.com>
2026-05-20 17:22:09 +0530

    bubble sort

---
+++ sorting/BubbleSort.py
@@ -0,0 +1,31 @@
+numbers = [8,10,6,2,4]
+n = len(numbers)
+count = 0
+for i in range(len(numbers)-1):
+    for j in range(len(numbers)- 1):
+        count += 1
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print(numbers)
+print(count)
+
+# numbers = [1,2,3,4,5] #[8,10,6,2,4]
+# n = len(numbers)
+# count = 0
+# while swapped:
+#     swapped = False
+#     for i in range(len(numbers)-1):
+#         for j in range(len(numbers)- 1):
+#             count += 1
+#             if numbers[j] > numbers[j + 1]:
+#                 swapped = True
+#                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+# print(numbers)
+# print(count)
+
+#   sort
+# my_list = [8, 10, 6, 2, 4]
+# my_list.sort()
+# print(my_list)
