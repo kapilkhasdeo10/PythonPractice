d8718567e8579113a8749f5e3fe278920d2cec0d
Garima <garimakhasdeo1610@gmail.com>
2026-05-30 21:11:00 +0530

    class

--- Oops/class.py
+++ Oops/class.py
@@ -15,20 +15,23 @@
 
 
 
-class student:
-    def __init__(self, name, age, gender, grade):
-        self.name = name
-        self.age = age
-        self.gender = gender
-        self.grade = grade
+# class student:
+#     def __init__(self, name, age, gender, grade):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#         self.grade = grade
         
-    def printdetails(self):
-        print("name:",self.name)
-        print("age:",self.age)
-        print("gender:",self.gender)
-        print("grade:",self.grade)
+#     def printdetails(self):
+#         print("name:",self.name)
+#         print("age:",self.age)
+#         print("gender:",self.gender)
+#         print("grade:",self.grade)
 
-kapil = student("kapil", 20, "male", "12th")
-print(kapil)
+# kapil = student("kapil", 20, "male", "12th")
+# print(kapil)
 
-kapil.printdetails()
+# kapil.printdetails()
+
+
+
