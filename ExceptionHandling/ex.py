d4b44defb74df6dd0b4653176b418222bc679c3e
Garima <garimakhasdeo1610@gmail.com>
2026-06-09 17:41:21 +0530

    inheritance

---
+++ ExceptionHandling/ex.py
@@ -0,0 +1,30 @@
+# def reciprocal(n):
+#     try:
+#         n = 1 / n
+#     except ZeroDivisionError:
+#         print("Division failed")
+#         return None
+#     else:
+#         print("Everything went fine")
+#         return n
+    
+# print("-----------")
+# print("reciprocal(2):", reciprocal(2))
+# print("-----------")
+# print("reciprocal(0):" , reciprocal(0))
+
+
+# def reciprocal(n):
+#     try:
+#         n = 1 / n
+#     except ZeroDivisionError:
+#         print("Division failed")
+#         n = None
+#     else:
+#         print("Everything went fine")
+#     finally:
+#         print("It's time to say goodbye")
+#         return n
+
+# print(reciprocal(2))
+# print(reciprocal(0))
