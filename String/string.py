04c2089024d910440412ad78e3aae178f52cec02
Garima <garimakhasdeo1610@gmail.com>
2026-06-10 17:40:57 +0530

    string

--- String/string.py
+++ String/string.py
@@ -30,4 +30,46 @@
 # print(text.find('python'))
 # print(text.count('t'))
 
+# csv = 'Kapil, 20, Indore, Engineer'
+# parts = csv.split(',')
+# print(parts)
+# print(parts[0])
+# rejoined = ' | '.join(parts)
+# print(rejoined)
+
+# print('hello123'.isalnum())
+# print('12345'.isdigit())
+# print('Python'.isalpha())
+# print(' '.isspace())
+
+# email = 'pythoncoding@gmail.com'
+# print(email.endswith('.com'))
+# print(email.startswith('pythoncoding'))
+
+
+# name, marks, rank = 'Kapil', 98.666, 3
+
+# print(f'Hello, (name)!')
+
+# print(f'marks:(marks:.2f)')
+# print(f'marks:(marks:.0f)')
+# print(f'count:(1000000:,)')
+
+# print(f'{name:<15}|{marks:>8.2f}|{rank:^13}')
+
+# price, get = 500, 0.18
+# print(f'Price:Rs.(price) | GST:Rs.(price*get:.2f)| Total.Rs.(price*(1*get):.2f)')
+
+
+# string = "Hello, How are you doing today?"
+# Vowel = {'a', 'e' ,'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+# count = 0
+# for vowel in string:
+#     if vowel in Vowel:
+#         count += 1 
+# print(count)
+
+
+
 
